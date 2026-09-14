ceda912d2ed9e300e65b8c769c9ff75c4c009b58
Roman Grekhov <grehovroman72@gmail.com>
2026-09-15 02:21:06 +0500

    fix: вывел имя файла в переменную FILE

--- script.sh
+++ script.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 TIME=300
+FILE=monitor.log
 while true; do
-	date +" --- %Y-%m-%d %H:%M:%S --- " >> monitor.log
+	date +" --- %Y-%m-%d %H:%M:%S --- " >> $FILE
 
-	echo -e "\n" >> monitor.log
-	free -h >> monitor.log
+	echo -e "\n" >> $FILE
+	free -h >> $FILE
 
-	echo -e "\n" >> monitor.log
-	df -h >> monitor.log
+	echo -e "\n" >> $FILE
+	df -h >> $FILE
 
-	echo -e "\n" >> monitor.log
-	uptime >> monitor.log
+	echo -e "\n" >> $FILE
+	uptime >> $FILE
 
-	echo -e "\n" >> monitor.log
+	echo -e "\n" >> $FILE
 
 	sleep $TIME
 done
